680b24c83322b94f78e2f0a9c9587e7e3e28ed62
DXBau <162867867+XuanBau011205@users.noreply.github.com>
2026-04-06 12:22:33 +0000

    Thêm code Spring Boot template

---
+++ src/main/java/com/example/iot_dashboard/IotDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.iot_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iot_dashboard/IotDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iot_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
